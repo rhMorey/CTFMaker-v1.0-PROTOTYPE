1048fe840e64da35d39799cfc05d3af463399e99
Ryad <96354755+rhMorey@users.noreply.github.com>
2023-02-02 20:51:56 +0100

    Starting

--- src/main/java/moreyctf/mcctf/Main.java
+++ src/main/java/moreyctf/mcctf/Main.java
@@ -1,13 +1,20 @@
 package moreyctf.mcctf;
 
+import moreyctf.mcctf.events.MainEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    public static Main plugin;
+
     @Override
     public void onEnable() {
 
+        plugin = this;
         System.out.println("CTF Maker is enabled!");
+        Bukkit.getPluginManager().registerEvents(new MainEvents(), this);
+
     }
 
     @Override
--- src/main/java/moreyctf/mcctf/events/MainEvents.java
+++ src/main/java/moreyctf/mcctf/events/MainEvents.java
@@ -6,14 +6,20 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+
+import java.util.Objects;
 
 public class MainEvents implements Listener {
 
     @EventHandler
     public void onCapture(PlayerInteractEvent event) {
 
+        Material clicked = Objects.requireNonNull(event.getClickedBlock()).getType();
+        if (Objects.equals(event.getHand(), EquipmentSlot.OFF_HAND)) return;
         Player player = event.getPlayer();
-        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && player.getTargetBlock(null, 5).getType().equals(Material.WHITE_BANNER)) {
+        if (event.getClickedBlock() == null) return;
+        if(event.getAction() == Action.RIGHT_CLICK_BLOCK && clicked.equals(Material.WHITE_BANNER)) {
             player.sendMessage("You captured the flag!");
         }
     }
